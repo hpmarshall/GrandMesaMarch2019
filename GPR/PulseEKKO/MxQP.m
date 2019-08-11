ec8ebadad25e488e992cd2771700cbc871722e20
tatemeehan <tatemeehan@u.boisestate.edu>
2019-08-11 12:54:32 -0600

    Intoduced Quad-Pol workflow.

---
+++ GPR/PulseEKKO/MxQP.m
@@ -0,0 +1,267 @@
+%% Multi-channel Quad-Pole GPR (MxQP)
+clear; close all; clc;
+%% Meta Data is a structure MD
+MD.dataDir = '/home/tatemeehan/GrandMesa2019/March27';
+addpath './functions';
+addpath './colormaps';
+MD.workDir = pwd;
+MD.fileNames = dir([MD.dataDir,'/','*.nc']);
+
+MD.lineNo = [1,2];                   % Array of data "LINE" numbers
+MD.nFiles = length(MD.lineNo);        % Number of Files
+nChan = 4;                      % Number of Recorded Channels
+chan =  1:nChan;                % Linear Array of Record Channels
+
+% Establish Tx Rx Geometry for CMP Gathering
+MD.nTx = 3;               % Number of Transmitters in Sequence
+MD.nRx = 3;               % Number of Receivers in Sequence
+%% Processing WorkFlow Controls
+% Parallel Computing Enabled
+isParallel = 1;
+% Read Data
+isReadNC = 1;                  % Read Multiplexed Data
+isTrimTWT = 0;          % Truncate Recorded Data
+isReduceData = 0;
+
+% Load Color Maps
+yetBlack = load('yetBlack.txt');
+%% Control Parallelization
+if isParallel
+    % Wake Parallel Computing
+    if isempty(gcp('nocreate'))     
+        nWorkers = nChan;
+        p = parpool(nWorkers);
+    else nWorkers = nChan;
+    end
+else
+    nWorkers = 1;
+end
+
+%% Read & Process GPR Data
+if isReadNC
+    Preprocess    
+end
+
+%% Measure Ground Reflection Coherency
+fprintf('Calculating Quad-Pole Coherence \n')
+tic
+
+QPcoherence
+
+toc
+%% Pick Ground Return
+GroundPicker
+%% AGC Gain
+isAGCgain = 0;
+if isAGCgain
+D.RadarAGC = cell(MD.nChan,MD.nFiles);
+for ii = 1:MD.nFiles
+    for jj = 1:MD.nChan
+        D.RadarAGC{jj,ii} = AGCgain(D.Radar{jj,ii},250,2);
+    end
+end
+end
+%% Calculate SWE
+for ii =  1:MD.nFiles
+L = 11;
+D.snowDensity = .250;
+D.velocity = DryCrimVRMS(D.snowDensity);
+D.snowDepth{ii} = (D.Time2Ground{ii}.*D.velocity)./2;
+D.SWE{ii} = D.snowDensity.*D.snowDepth{ii};
+% Smooth SWE
+% D.SWE{ii} = movmean(D.snowDensity.*D.snowHeight,L);
+end
+
+%% Sum Radar Energy above Ground
+
+% Sum energy of Coherency Grams
+% for ii =  1:MD.nFiles
+%     groundIx = D.groundIx{ii};
+%     Co = D.HHHVCoherence{ii};
+%     Cx = D.VVVHCoherence{ii};
+%     tmp = zeros(size(D.Radar{1,ii},2),1);
+%     parfor (kk = 1:size(D.Radar{1,ii},2),nWorkers)
+%         tmp(kk) = sum(Cx(1:(groundIx(kk)-deconIx),kk))./sum(Co(1:(groundIx(kk)-deconIx),kk));
+%     end
+%     D.EnergyRatio{ii} = movmean(tmp,5.*L);
+% end
+% tic
+% energySum = cell(MD.nChan,MD.nFiles);
+% EnergyRatio = cell(MD.nChan,MD.nFiles);
+% Rad= D.Radar;
+% rmIx = 25; % Remove 25 samples above coherent reflector
+% for ii = 1:MD.nFiles
+% %     rad = D.Radar{jj,ii};
+%     for jj = 1:MD.nChan
+%         for kk = 1:size(D.Radar{jj,ii},2)
+% %             energySum{jj,ii}(kk) = sum((Rad{jj,ii}([1:D.groundIx(:)-rmIx],kk).^2));
+%             energySum{jj,ii}(kk) = sum((Rad{jj,ii}(:,kk).^2));
+%         end
+%     end
+%     for jj = 1:MD.nChan
+%         if strcmp(D.Polarization{ii,jj},'HH')
+%             % Cross-Pole HV to Co-Pole HH
+%             EnergyRatio{jj,ii} = energySum{1,ii}./energySum{jj,ii};
+%         elseif strcmp(D.Polarization{ii,jj},'HV')
+%             % HV to VV
+%             EnergyRatio{jj,ii} = energySum{jj,ii}./energySum{3,ii};
+%         elseif strcmp(D.Polarization{ii,jj},'VV')
+%             % VH to VV
+%             EnergyRatio{jj,ii} = energySum{4,ii}./energySum{jj,ii};
+%         elseif strcmp(D.Polarization{ii,jj},'VH')
+%             % VH to HH
+%             EnergyRatio{jj,ii} = energySum{jj,ii}./energySum{2,ii};
+%         else
+%             error('Polarizations are not Defined!')
+%         end 
+%     end
+% end
+% toc
+%% Make Figures
+set(0,'DefaultAxesFontName','FreeSerif')
+set(0,'DefaultTextFontName','FreeSerif')
+% set(0,'DefaultFontSize',14,'DefaultFontWeight','bold')
+xix = [find(D.DistanceAxis>3700,1):find(D.DistanceAxis>5300,1)];
+% yix = [1:300;
+% xix = 1:length(D.DistanceAxis);
+figure();
+pcolor(D.DistanceAxis(xix),D.TimeAxis,D.Coherence{1}(:,xix));
+shading interp; colormap(yetBlack);axis ij;hold on;caxis([0,1]);
+% imagesc(D.DistanceAxis(xix),D.TimeAxis,D.Coherence{1}(:,xix));
+% shading interp; colormap(yetBlack);hold on;
+plot(D.DistanceAxis(xix),D.Time2Ground(xix)-1,'.','color',[.7,.7,.7])
+c = colorbar; c.Location = 'northoutside';c.Label.String = 'Quad-Pol Coherence';
+c.FontSize = 12; c.Label.FontSize = 14;
+set(gca,'fontsize',14,'fontweight','bold','layer','top')
+%          title('Coherence')
+ylabel('Travel-time (ns)')
+xlabel('Distance (m)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+figure();
+pcolor(D.DistanceAxis(xix),D.TimeAxis,D.HHHVCoherence{1}(:,xix));
+shading interp; colormap(yetBlack);axis ij;hold on;caxis([0,1]);
+plot(D.DistanceAxis(xix),D.Time2Ground(xix)-1,'.','color',[.7,.7,.7])
+c = colorbar; c.Location = 'northoutside';c.Label.String = 'HH-HV Coherence';
+c.FontSize = 12; c.Label.FontSize = 14;
+set(gca,'fontsize',14,'fontweight','bold','layer','top')
+%          title('Coherence')
+ylabel('Travel-time (ns)')
+xlabel('Distance (m)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+figure();
+pcolor(D.DistanceAxis(xix),D.TimeAxis,D.VVVHCoherence{1}(:,xix));
+shading interp; colormap(yetBlack);axis ij;hold on;
+plot(D.DistanceAxis(xix),D.Time2Ground(xix)-2.5,'.','color',[.7,.7,.7])
+c = colorbar; c.Location = 'northoutside';c.Label.String = 'Cross-Pol Coherence';
+c.FontSize = 12; c.Label.FontSize = 14;
+set(gca,'fontsize',14,'fontweight','bold','layer','top')
+%          title('Coherence')
+ylabel('Travel-time (ns)')
+xlabel('Distance (m)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+% Plot Time Gain RadarGram HV
+cq = quantile(D.Radar{1}(:),[0.005,0.995]);
+figure();
+pcolor(D.DistanceAxis,D.TimeAxis,D.Radar{1,1});
+shading interp;colormap(bone);axis ij;
+caxis(cq);
+set(gca,'fontsize',14,'fontweight','bold','layer','top')
+title('HV Polarization')
+ylabel('Travel-time (ns)')
+xlabel('Distance (m)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+% Plot Time Gain RadarGram HH
+cq = quantile(D.Radar{2}(:),[0.005,0.995]);
+figure();
+pcolor(D.DistanceAxis,D.TimeAxis,D.Radar{2,1});
+shading interp;colormap(bone);axis ij;
+caxis(cq);
+set(gca,'fontsize',14,'fontweight','bold','layer','top')
+title('HH Polarization')
+ylabel('Travel-time (ns)')
+xlabel('Distance (m)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+% Plot Time Gain RadarGram VV
+cq = quantile(D.Radar{3}(:),[0.005,0.995]);
+figure();
+pcolor(D.DistanceAxis,D.TimeAxis,D.Radar{3,1});
+shading interp;colormap(bone);axis ij;
+caxis(cq);
+set(gca,'fontsize',14,'fontweight','bold','layer','top')
+title('VV Polarization')
+ylabel('Travel-time (ns)')
+xlabel('Distance (m)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+% Plot Time Gain RadarGram VH
+cq = quantile(D.Radar{4}(:),[0.005,0.995]);
+figure();
+pcolor(D.DistanceAxis,D.TimeAxis,D.Radar{4,1});
+shading interp;colormap(bone);axis ij;
+caxis(cq);
+set(gca,'fontsize',14,'fontweight','bold','layer','top')
+title('VH Polarization')
+ylabel('Travel-time (ns)')
+xlabel('Distance (m)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+% Thin Results
+corrIx = find(D.EnergyRatio{1}(xix)>quantile(D.EnergyRatio{1}(xix),.05) & D.EnergyRatio{1}(xix)<quantile(D.EnergyRatio{1}(xix),.95));
+figure();
+scatter(D.EnergyRatio{1}(xix(corrIx)),D.SWE(xix(corrIx)),'k'); hold on;
+lsline;
+[R,P] = corrcoef(D.EnergyRatio{1}(xix(corrIx)),D.SWE(xix(corrIx)));
+annotation('textbox',[.8,.8,.1,.1],'string',{['R^2: ',num2str(R(1,2).^2)]},'FitBoxToText','on');
+% ;['p: ',num2str(P)]
+
+figure();plot(D.DistanceAxis(xix),D.EnergyRatio{1}(xix),'.k')
+hold on; plot(D.DistanceAxis(xix),D.SWE(xix),'.r')
+set(gca,'fontsize',14,'fontweight','bold')
+title('HV/HH Energy Ratio and SWE')
+xlabel('Distance (m)')
+legend('HV/HH','SWE','location','NorthWest')
+
+figure();
+scatter(EnergyRatio{1,1}(xix),D.SWE(xix),'k');hold on; lsline
+[R,P] = corrcoef(EnergyRatio{1,1}(xix),D.SWE(xix));
+annotation('textbox',[.7,.2,.1,.1],'string',{['R^2: ',num2str(R(1,2).^2)]},'FitBoxToText','on','LineStyle','none','fontsize',14);
+title('HV/VV')
+ylabel('SWE')
+xlabel('Energy Ratio (HV/VV)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+figure();
+scatter(EnergyRatio{2,1}(xix),D.SWE(xix),'k'); hold on; lsline
+[R,P] = corrcoef(EnergyRatio{2,1}(xix),D.SWE(xix));
+annotation('textbox',[.7,.2,.1,.1],'string',{['R^2: ',num2str(R(1,2).^2)]},'FitBoxToText','on','LineStyle','none','fontsize',14);
+title('HV/HH')
+ylabel('SWE')
+xlabel('Energy Ratio (HV/HH)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+
+figure();
+scatter(EnergyRatio{3,1}(xix),D.SWE(xix),'k'); hold on; lsline
+[R,P] = corrcoef(EnergyRatio{3,1}(xix),D.SWE(xix));
+annotation('textbox',[.7,.2,.1,.1],'string',{['R^2: ',num2str(R(1,2).^2)]},'FitBoxToText','on','LineStyle','none','fontsize',14);
+
+title('VH/VV')
+ylabel('SWE')
+xlabel('Energy Ratio (VH/VV)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+figure();
+scatter(EnergyRatio{4,1}(xix),D.SWE(xix),'k');hold on;lsline
+[R,P] = corrcoef(EnergyRatio{4,1}(xix),D.SWE(xix));
+annotation('textbox',[.7,.2,.1,.1],'string',{['R^2: ',num2str(R(1,2).^2)]},'FitBoxToText','on','LineStyle','none','fontsize',14);
+title('VH/HH')
+ylabel('SWE')
+xlabel('Energy Ratio (VH/HH)')
+set(gca,'fontsize',14,'fontweight','bold')
+
+
---
+++ GPR/PulseEKKO/functions/SVDSfilter.m
@@ -0,0 +1,43 @@
+function [dataOut] = SVDSfilter(data,threshold,BPix)
+% SVDSfilter performs an economical decomposition of the GPR data. Using
+% the bandpass method (Cagnoli and Ulrych 2001) coherent noise and clutter
+% in the form of eigenimages are removed from the data. The first 80
+% singular values are requested for a scalable SVD filter. The pass-band 
+% may be specified by the user, or will be determined by PCA.
+%
+% Tate Meehan 12/4/2018
+% Surpress Try Catch Warning
+id = 'MATLAB:mir_warning_changing_try_catch';
+warning('off',id);
+try threshold;
+catch
+    threshold = 0.5;
+end
+try BPix;
+catch
+    % Set the Default Decomposition to 100 singular values
+    BPix = [0,100];
+end
+% [U,S,V] = svd(data);
+[U,S,V] = svds(data,BPix(2));
+% Extract the Singular Values for PCA
+Sigma = diag(S);           
+% Principle Components of SVD
+pca = Sigma./sum(Sigma);
+% The cumulative PCA  value determines the filter Low Cut
+% Coherent Noise is beleived to contribute between 25% - 50% of EigenImage
+if BPix(1) == 0
+    cumPCA = tril(ones(length(pca)))*pca;
+    BPix(1) = find(cumPCA >=threshold,1);
+end
+% Weight for Low Cut Eigenvalue > threshold
+w = (cumPCA(BPix(1)) - threshold)./threshold;
+% Kill the Largest Eigenvalues
+if BPix(1) > 1
+S(1:BPix(1)-1,1:BPix(1)-1) = 0; 
+end
+S(BPix(1),BPix(1)) = w;
+% Reconstruct the Data
+dataOut = U * S * V'; 
+end
+
